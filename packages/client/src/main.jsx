c69ba9770af8415e73ff64153de0533dcab24e42
Jase Kraft <jasekraft12@gmail.com>
2024-11-22 13:09:09 -0600

    react query on shopping list

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const theme = createTheme();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
